b4ca45b3393c6c45e4eb6abf410efac0a628c4b9
Kemboijosh <kemboijoshua883@gmail.com>
2026-04-01 13:01:37 +0300

    done

--- edumarket-backend/app.py
+++ edumarket-backend/app.py
@@ -4,6 +4,7 @@ import json
 import time
 from flask import Flask, request, jsonify
 from flask_cors import CORS
+import os
 
 app = Flask(__name__)
 CORS(app)
@@ -163,4 +164,6 @@ def callback():
     return jsonify({"ResultCode": 0, "ResultDesc": "Accepted"})
 
 if __name__ == '__main__':
-    app.run(debug=True, port=8080)
+    import os
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host="0.0.0.0", port=port, debug=False)
